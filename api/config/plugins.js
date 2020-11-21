188f635c7eb584e6ca61b5e210f625ea947f8b2b
Dulguun Battulga <dolugen@gmail.com>
2020-11-21 21:21:41 +0100

    increase graphql amountLimit

--- api/config/plugins.js
+++ api/config/plugins.js
@@ -10,4 +10,14 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  graphql: {
+    endpoint: "/graphql",
+    shadowCRUD: true,
+    playgroundAlways: false,
+    depthLimit: 7,
+    amountLimit: 10000, // default: 100
+    apolloServer: {
+      tracing: false,
+    },
+  },
 });
